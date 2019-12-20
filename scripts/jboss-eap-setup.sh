c4c17e19b97375be5bb820595bfe8a853184219c
Guy Bowerman <guybo@outlook.com>
2019-12-19 16:57:12 -0800

    Change how IP_ADDR calculated

--- scripts/jboss-eap-setup.sh
+++ scripts/jboss-eap-setup.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 # JBOSS EAP clustered setup script
-# arguments:
-#  $1 storage account name
-#  $2 storage access key
 export STORAGE_ACCOUNT_NAME=$1
 export STORAGE_ACCESS_KEY=$2
-export IP_ADDR=$3
 export EAP_HOME==/opt/rh/eap7/root/usr/share/wildfly
 cp $EAP_HOME/docs/examples/configs/standalone-azure-ha.xml $EAP_HOME/standalone/configuration/standalone-ha.xml
 firewall-offline-cmd --add-port=8080/tcp
@@ -15,6 +11,11 @@ firewall-offline-cmd --add-port=10090/tcp
 systemctl enable firewalld
 firewall-cmd --reload
 
+# get IP address
+wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+chmod +x ./jq
+cp jq /usr/bin
+IP_ADDR=`curl -H Metadata:true "http://169.254.169.254/metadata/instance/network?api-version=2017-03-01" | jq .interface[0].ipv4.ipaddress[0].ipaddress`
 # For now startup manually
 # To do - edit the service and restart
 $EAP_HOME/bin/standalone.sh -b $IP_ADDR -bprivate $IP_ADDR --server-config=standalone-ha.xml -Djboss.jgroups.azure_ping.storage_account_name=$STORAGE_ACCOUNT_NAME -Djboss.jgroups.azure_ping.storage_access_key=$STORAGE_ACCESS_KEY -Djboss.jgroups.azure_ping.container=jgroupcontainer > /dev/null 2>&1 &
