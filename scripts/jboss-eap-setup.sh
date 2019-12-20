a7ac75ca7b2ee2d91a0f741c6fccf6fb0e541259
Guy Bowerman <guybo@outlook.com>
2019-12-19 17:42:06 -0800

    Update jboss-eap-setup.sh
    Start firewall later

--- scripts/jboss-eap-setup.sh
+++ scripts/jboss-eap-setup.sh
@@ -8,8 +8,6 @@ firewall-offline-cmd --add-port=8080/tcp
 firewall-offline-cmd --add-port=8180/tcp
 firewall-offline-cmd --add-port=9990/tcp
 firewall-offline-cmd --add-port=10090/tcp
-systemctl enable firewalld
-firewall-cmd --reload
 
 # get IP address
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
@@ -19,3 +17,5 @@ IP_ADDR=`curl -H Metadata:true "http://169.254.169.254/metadata/instance/network
 # For now startup manually
 # To do - edit the service and restart
 $EAP_HOME/bin/standalone.sh -b $IP_ADDR -bprivate $IP_ADDR --server-config=standalone-ha.xml -Djboss.jgroups.azure_ping.storage_account_name=$STORAGE_ACCOUNT_NAME -Djboss.jgroups.azure_ping.storage_access_key=$STORAGE_ACCESS_KEY -Djboss.jgroups.azure_ping.container=jgroupcontainer > /dev/null 2>&1 &
+systemctl enable firewalld
+firewall-cmd --reload
