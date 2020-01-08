96df6d7fbda64b9a8945d05998b8a64cfac410ef
Guy Bowerman <guybo@outlook.com>
2020-01-07 17:17:35 -0800

    revert IP addr change

--- scripts/jboss-eap-setup.sh
+++ scripts/jboss-eap-setup.sh
@@ -17,7 +17,7 @@ wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x ./jq
 cp jq /usr/bin
 export IP_ADDR=`curl -H Metadata:true "http://169.254.169.254/metadata/instance/network?api-version=2017-03-01" | jq .interface[0].ipv4.ipaddress[0].ipaddress | sed -e 's/^"//' -e 's/"$//'`
-export PUB_IP_ADDR=`curl -H Metadata:true "http://169.254.169.254/metadata/instance/network?api-version=2017-03-01" | jq .interface[0].ipv4.ipaddress[0].publicip | sed -e 's/^"//' -e 's/"$//'`
+# export PUB_IP_ADDR=`curl -H Metadata:true "http://169.254.169.254/metadata/instance/network?api-version=2017-03-01" | jq .interface[0].ipv4.ipaddress[0].publicip | sed -e 's/^"//' -e 's/"$//'`
 # For now startup manually
 # To do - edit the service and restart
-$EAP_HOME/bin/standalone.sh -b $PUB_IP_ADDR -bprivate $IP_ADDR -bmanagement $PUB_IP_ADDR --server-config=standalone-ha.xml -Djboss.jgroups.azure_ping.storage_account_name=$STORAGE_ACCOUNT_NAME -Djboss.jgroups.azure_ping.storage_access_key=$STORAGE_ACCESS_KEY -Djboss.jgroups.azure_ping.container=jgroupcontainer > /var/log/eap7.log 2>&1 &
+$EAP_HOME/bin/standalone.sh -b $IP_ADDR -bprivate $IP_ADDR -bmanagement $IP_ADDR --server-config=standalone-ha.xml -Djboss.jgroups.azure_ping.storage_account_name=$STORAGE_ACCOUNT_NAME -Djboss.jgroups.azure_ping.storage_access_key=$STORAGE_ACCESS_KEY -Djboss.jgroups.azure_ping.container=jgroupcontainer > /var/log/eap7.log 2>&1 &
